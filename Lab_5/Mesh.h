501d6b6dcc24ba4dc5fdb7c1260a22c0f614b02a
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-04 03:25:14 +0800

    Добавлены Mesh.h и Model.h из папки assimp и пустые Model.cpp и Mesh.cpp

---
+++ Lab_5/Mesh.h
@@ -0,0 +1,41 @@
+// На основе Mesh.h из папки assimp
+
+#ifndef MESH_H
+#define MESH_H
+
+#include <GL/glew.h> 
+#include <glm.hpp>   
+#include <gtc/matrix_transform.hpp>
+
+#include <vector>
+
+using namespace std;
+
+struct Vertex {
+    // position
+    glm::vec3 Position;
+    // normal
+    glm::vec3 Normal;      
+};
+
+class Mesh {
+public:
+    vector <Vertex> vertices; 
+    vector <unsigned int> indices;
+    unsigned int VAO;
+
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices)
+    {}
+
+    void Draw()
+    {}
+
+private:
+    unsigned int VBO, EBO;
+
+    void setupMesh()
+    {}
+};
+#endif
+
+
